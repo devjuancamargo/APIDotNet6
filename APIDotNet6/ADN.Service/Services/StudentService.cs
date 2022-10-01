6134c1f4112f62fff1678bc0c914bc594ad82077
Juan Carlos Gonçalves Camargo <juan.camargo@mkth.com.br>
2022-10-01 02:54:54 -0300

    Criando Service

---
+++ APIDotNet6/ADN.Service/Services/StudentService.cs
@@ -0,0 +1,13 @@
+﻿using ADN.Domain.Interfaces.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADN.Service.Services
+{
+    internal class StudentService : IStudentService
+    {
+    }
+}
