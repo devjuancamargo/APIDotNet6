f10cf9ce6525e0d433512177c959eaa34bcad7a3
Juan Carlos Gonçalves Camargo <juan.camargo@mkth.com.br>
2022-10-01 02:43:57 -0300

    Gerando Data

--- APIDotNet6/ADN.Data/Repository/StudentRepository.cs
+++ APIDotNet6/ADN.Data/Repository/StudentRepository.cs
@@ -1,6 +1,28 @@
-﻿namespace ADN.Data.Repository
+﻿using ADN.Domain.Domain;
+using ADN.Domain.DTO.Settings;
+using ADN.Domain.Interfaces.Repository;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ADN.Data.Repository
 {
-    public class StudentRepository
+    public class StudentRepository : IStudentRepository
     {
+        private readonly IMongoCollection<Student> _collection;
+
+
+        public StudentRepository(IOptions<MongoDbStudentSettings> mongoStudentSettings)
+        {
+            var mongoClient = new MongoClient(mongoStudentSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(mongoStudentSettings.Value.DatabaseName);
+            _collection = mongoDatabase.GetCollection<Student>(mongoStudentSettings.Value.CollectionName);
+
+        }
+
+        public async Task<List<Student>> GetAll()
+        {
+            var result = await _collection.FindAsync(c => true);
+            return result.ToList();
+        }
     }
 }
--- APIDotNet6/ADN.Domain/Interfaces/Repository/IStudentRepository.cs
+++ APIDotNet6/ADN.Domain/Interfaces/Repository/IStudentRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace ADN.Domain.Interfaces.Repository
+﻿using ADN.Domain.Domain;
+
+namespace ADN.Domain.Interfaces.Repository
 {
     public interface IStudentRepository
     {
+        Task<List<Student>> GetAll();
     }
 }
